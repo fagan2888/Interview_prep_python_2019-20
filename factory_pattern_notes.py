b7c2009a061d85a422cb97c9103c2fdd2ab8aa3f
Liwen Huang <liwenhuang6@gmail.com>
2020-02-02 15:26:40 -0800

    factory pattern

---
+++ factory_pattern_notes.py
@@ -0,0 +1,171 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb  2 14:25:43 2020
+
+@author: liwenhuang
+"""
+
+"""
+factory pattern notes
+https://realpython.com/factory-method-python/
+*when code implementation depends on a set of complex if/elif/else conditions
+*think about seperate the different paths of implementation
+The pattern removes complex logical code that is hard to maintain, and replaces it with a design that is reusable and extensible. The pattern avoids modifying existing code to support new requirements.
+"""
+
+# In serializer_demo.py
+
+import json
+import xml.etree.ElementTree as et
+
+class Song:
+    def __init__(self, song_id, title, artist):
+        self.song_id = song_id
+        self.title = title
+        self.artist = artist
+
+
+class SongSerializer:
+    def serialize(self, song, format):
+        if format == 'JSON':
+            song_info = {
+                'id': song.song_id,
+                'title': song.title,
+                'artist': song.artist
+            }
+            return json.dumps(song_info)
+        elif format == 'XML':
+            song_info = et.Element('song', attrib={'id': song.song_id})
+            title = et.SubElement(song_info, 'title')
+            title.text = song.title
+            artist = et.SubElement(song_info, 'artist')
+            artist.text = song.artist
+            return et.tostring(song_info, encoding='unicode')
+        else:
+            raise ValueError(format)
+
+"""
+the following provides the basic implementation of factory method
+*identify common goal of each of the execution paths -- serialize a song to string format
+*provide a separate component that decides the concrete implementation to use based on the specified format
+"""
+class SongSerializer:
+    def serialize(self, song, format):
+        serializer = self._get_serializer(format)
+        return serializer(song)
+
+    def _get_serializer(self, format):
+        if format == 'JSON':
+            return self._serialize_to_json
+        elif format == 'XML':
+            return self._serialize_to_xml
+        else:
+            raise ValueError(format)
+
+#Note: The ._get_serializer() method does not call the concrete implementation, and it just returns the function object itself.
+
+    def _serialize_to_json(self, song):
+        payload = {
+            'id': song.song_id,
+            'title': song.title,
+            'artist': song.artist
+        }
+        return json.dumps(payload)
+
+    def _serialize_to_xml(self, song):
+        song_element = et.Element('song', attrib={'id': song.song_id})
+        title = et.SubElement(song_element, 'title')
+        title.text = song.title
+        artist = et.SubElement(song_element, 'artist')
+        artist.text = song.artist
+        return et.tostring(song_element, encoding='unicode')
+    
+"""
+note how get_serializer can be an external function
+"""    
+class SongSerializer:
+    def serialize(self, song, format): #the application code that depends on an interface to complete its task. so called client component of the pattern
+        serializer = get_serializer(format)
+        return serializer(song)
+
+
+def get_serializer(format): #creator component
+    if format == 'JSON':
+        return _serialize_to_json
+    elif format == 'XML':
+        return _serialize_to_xml
+    else:
+        raise ValueError(format)
+
+
+def _serialize_to_json(song):
+    payload = {
+        'id': song.song_id,
+        'title': song.title,
+        'artist': song.artist
+    }
+    return json.dumps(payload)
+
+
+def _serialize_to_xml(song):
+    song_element = et.Element('song', attrib={'id': song.song_id})
+    title = et.SubElement(song_element, 'title')
+    title.text = song.title
+    artist = et.SubElement(song_element, 'artist')
+    artist.text = song.artist
+    return et.tostring(song_element, encoding='unicode')
+
+
+"""
+A general interface 
+"""
+# In serializers.py
+
+import json
+import xml.etree.ElementTree as et 
+
+class JsonSerializer:
+    def __init__(self):
+        self._current_object = None
+
+    def start_object(self, object_name, object_id):
+        self._current_object = {
+            'id': object_id
+        }
+
+    def add_property(self, name, value):
+        self._current_object[name] = value
+
+    def to_str(self):
+        return json.dumps(self._current_object)
+
+
+class XmlSerializer:
+    def __init__(self):
+        self._element = None
+
+    def start_object(self, object_name, object_id):
+        self._element = et.Element(object_name, attrib={'id': object_id})
+
+    def add_property(self, name, value):
+        prop = et.SubElement(self._element, name)
+        prop.text = value
+
+    def to_str(self):
+        return et.tostring(self._element, encoding='unicode')
+
+# In object_factory.py
+
+class ObjectFactory:
+    def __init__(self):
+        self._builders = {}
+
+    def register_builder(self, key, builder):
+        self._builders[key] = builder
+
+    def create(self, key, **kwargs):
+        builder = self._builders.get(key)
+        if not builder:
+            raise ValueError(key)
+        return builder(**kwargs)
