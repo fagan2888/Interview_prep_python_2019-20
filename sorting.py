7b203fdf7d95c59d6a5a35ac7e90c5e241b1e3c9
Liwen Huang <liwenhuang6@gmail.com>
2020-04-20 16:03:14 -0700

    merge sort and quick sort redone

--- sorting.py
+++ sorting.py
@@ -16,149 +16,67 @@ def bubble_sort(nums):
         n -= 1
 
 
-# merge two sorted lists
-nums1 =[2, 7, 9, 11, 15]
-nums2 =[2, 4, 5]
-
-def merge(nums1, nums2):
-    arr = []
-    while nums1 and nums2:
-        if nums1[0] <= nums2[0]:
-            num = nums1.pop(0)
-        else:
-            num = nums2.pop(0)
-        arr.append(num)
-
-    if nums1:
-        arr.extend(nums1)
-    if nums2:
-        arr.extend(nums2)
-
-    return arr
-
-# merge two sorted array in place
-nums1 = [1,2,3]
-m = 3
-nums2 = [2,5,6]
-n = 3
-
-def merge(nums1, m, nums2, n):
-        """
-        Do not return anything, modify nums1 in-place instead.
-        """
-        for num in nums2:
-            for i in range(m):
-                if nums1[i] <= num:
-                    left = i
-                if nums1[i] >= num:
-                    right = i
-            if left:
-                nums1.insert(left+1, num)
-            else:
-                nums1.insert(right-1, num)
-            print(nums1)
-            m += 1
-
-#merge(nums1, m, nums2, n)
-
-# merge two sorted linklist
-# use recursion
-def mergeTwoLists2(l1, l2):
-        if not l1 or not l2:
-            return l1 or l2
-        if l1.val < l2.val:
-            l1.next = mergeTwoLists(l1.next, l2)
-            return l1
-        else:
-            l2.next = mergeTwoLists(l1, l2.next)
-            return l2
-
-# return a new list
-def mergeTwoLists(l1, l2):
-
-    dummy = pointer = ListNode(0)
-
-    while l1 and l2:
-        if l1.val <= l2.val:
-            pointer.next = l1
-            l1 = l1.next
-        else:
-            pointer.next = l2
-            l2 = l2.next
-        pointer = pointer.next
-
-    pointer.next = l1 or l2
-
-    return dummy.next
-
 """
- merge sort in a dumb way
+ theoretically merge sort is awesome for its perfect divide and conquer logics, no matter what happens it remains nlogn, but the disavantage is the O(n) extra space that is requires to do the merging
+ whenever a new array needs to be created and released, that takes up resources, therefore, in practice, quicksort may be actually better
+ a slight optmization for merge sort can be passing an array into the function as a parameter therefore the operation of creating a new array dose not need to be done multiple times
+ the following solution modify the original array instead, however, note that it dose not save the O(n) space
 """
-nums = [7, 8, 10, 5, 9, 7, 2]
+nums = [5, 2, 7, 9, 20, 1]
+# merge sort
 
 def merge_sort(nums):
-    def merge(nums1, nums2):
-        arr = []
-        while nums1 and nums2:
-            if nums1[0] <= nums2[0]:
-                num = nums1.pop(0)
-            else:
-                num = nums2.pop(0)
-            arr.append(num)
+    if len(nums) == 1:
+        return
+    mid = len(nums)//2
+    left, right = nums[:mid], nums[mid:] # O(n) space
+    merge_sort(left)
+    merge_sort(right)
 
-        if nums1:
-            arr.extend(nums1)
-        if nums2:
-            arr.extend(nums2)
-
-        return arr
-
-    n = len(nums)
-    if n <= 1:
-        return nums
-    mid = n//2
-    left = merge_sort(nums[:mid])
-    right = merge_sort(nums[mid:])
-    return merge(left, right)
-
-
-# solution from geekforgeek
-def mergeSort(arr):
-    if len(arr) >1:
-        mid = len(arr)//2 #Finding the mid of the array
-        L = arr[:mid] # Dividing the array elements
-        R = arr[mid:] # into 2 halves
-
-        mergeSort(L) # Sorting the first half
-        mergeSort(R) # Sorting the second half
-
-        i = j = k = 0
-
-        # Copy data to temp arrays L[] and R[]
-        while i < len(L) and j < len(R):
-            if L[i] < R[j]:
-                arr[k] = L[i]
-                i+=1
-            else:
-                arr[k] = R[j]
-                j+=1
-            k+=1
-
-        # Checking if any element was left
-        while i < len(L):
-            arr[k] = L[i]
-            i+=1
-            k+=1
-
-        while j < len(R):
-            arr[k] = R[j]
-            j+=1
-            k+=1
-
+    # modify the original nums array
+    k = 0
+    while left and right:
+        if left[0] < right[0]:
+            nums[k] = left.pop(0)
+        else:
+            nums[k] = right.pop(0)
+        k += 1
+    while left:
+        nums[k] = left.pop(0)
+        k += 1
+    while right:
+        nums[k] = right.pop(0)
+        k += 1
 
 
 """
-quick sort in a dumb way
+归并排序算法的步骤为：
+
+找出数组的中点
+将数组分成两个部分递归执行该算法，分别排序两个部分
+合并两个排序好的子数组到一个大数组
+写成T函数为：T(n) = 2*T(n/2) + O(n)T(n)=2∗T(n/2)+O(n)，其中
+
+2*T(n/2)2∗T(n/2) 代表将 n 的问题，拆分为两个 n/2 的同类问题去进行处理。
+O(n)O(n) 代表了，合并两个排好序的 n/2 大小的数组的时间复杂度。
+我们用展开的方式推导该算法下的 T(n):
+
+T(n) = 2 * T(n/2) + O(n)
+     = 2 * (2 * T(n/4) + O(n/2)) + O(n)
+     = 4 * T(n/4) + 2 * O(n/2) + O(n)
+     = 4 * T(n/4) + 2 * O(n)
+     = 4 * (2 * T(n/8) + O(n/4)) + 2 * O(n)
+     = 8 * T(n/8) + 3 * O(n)
+     = 16 * T(n/16) + 4 * O(n)
+     ...
+     = n * T(1) + logn * O(n)
+     = O(n) + O(nlogn)
+     = O(nlogn)
+"""
+
+
+"""
+quick sort is pretty difficult for me to remember in details for some reasone, here I use the following dumb implementation to learn its logic
 """
 def quick_sort(nums):
     n = len(nums)
@@ -177,5 +95,63 @@ def quick_sort(nums):
 
     return quick_sort(lower) + [pivot] + quick_sort(higher)
 
+# here is the implementation of quicksort
+class Solution:
+    def sort(self, nums):
+        self.quick_sort(nums, 0, len(nums)-1)
 
-Sa
+    def quick_sort(self,nums, start, end):
+        if start >= end:
+            return
+
+        pivot = nums[(start+end)//2]
+        left, right = start, end
+
+        while left < right:
+            while nums[left] < pivot:
+                left += 1
+            while nums[right] > pivot:
+                right -= 1
+            if left < right:
+                nums[left], nums[right] =  nums[right], nums[left]
+                left += 1
+                right -= 1
+
+        # the position of the index: right before left
+        self.quick_sort(nums, start, right)
+        self.quick_sort(nums, left, end)
+
+
+nums = [5, 2, 7, 9, 20, 1]
+ans = Solution()
+ans.sort(nums)
+
+"""
+although quick sort build on top of quick select, the partition part is identical, but for some reason, it is really difficult to do it right, especially remember the right portion
+here is a template for me to remember
+note the different condition when using left < right or left <= right in the implementation, that result in different final positions of the left and right pointers
+"""
+# quick select, solve problem such as k-th element, find median, O(n) averaged time complexity
+def quick_select(nums, k, start, end):
+    if start == end:
+        return nums[start]
+
+    left, right = start, end
+    pivot = nums[(start + end)//2]
+    while left <= right:
+        while nums[left] < pivot:
+            left += 1
+        while nums[right] > pivot:
+            right -= 1
+        if left <= right:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+            right -= 1
+
+    if start + (k-1) <= right:
+        return quick_select(nums, k, start, right)
+    if start + (k-1) >= left:
+        return quick_select(nums, k - (left-start), left, end)
+    return pivot
+
+quick_select(nums, 1, 0, len(nums)-1)
